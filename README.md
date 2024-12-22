fd1fc0ec754d4218023da7c874826d6487902c9f
Nyabsi <nyabsi@sovellus.cc>
2024-12-22 14:29:48 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pico Theme Manager
+
+Pico Theme Manager is a tool to help you easily switch between custom Pico OS themes on your Pico device.
+
+## Demo
+
+## Creating Custom Themes
+
+You can find guide for making your own custom themes [here](https://gist.github.com/Nyabsi/c14bd38d03d6dc44721779c182762627)
+
+## Contributing
+
+Feel free to contribute through Pull Request, if you think this project needs something else, feel free to make an issue.
+
+## License
+
+This project is licensed under MIT which can be found in `LICENSE` file in the root folder
---
+++ app/src/main/java/cc/sovellus/picothememanager/MainActivity.kt
@@ -0,0 +1,135 @@
+package cc.sovellus.picothememanager
+
+import android.annotation.SuppressLint
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK
+import android.content.pm.PackageInfo
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import cc.sovellus.picothememanager.ui.theme.ThemetoolTheme
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var context: Context
+
+    private fun restartShell() {
+        val intent = Intent()
+        intent.component = ComponentName("com.pvr.vrshell", "com.pvr.vrshell.MainActivity")
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+
+    private fun getPackageList(): List<PackageInfo> {
+        return packageManager.getInstalledPackages(0).filter {
+            it.packageName.contains(Regex("com.pvr.[^.]+.scene"))
+        }
+    }
+
+    @OptIn(ExperimentalGlideComposeApi::class)
+    @SuppressLint("DiscouragedApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        context = this
+
+        setContent {
+            ThemetoolTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(top = innerPadding.calculateTopPadding(), bottom = innerPadding.calculateBottomPadding()).fillMaxSize()
+                    ) {
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(4),
+                            contentPadding = PaddingValues(
+                                start = 12.dp,
+                                top = 16.dp,
+                                end = 16.dp,
+                                bottom = 16.dp
+                            ),
+                            content = {
+                                items(getPackageList()) {
+
+                                    val resources = packageManager.getResourcesForApplication(it.packageName)
+                                    val sceneTag = resources.getString(resources.getIdentifier("sceneTag", "string", it.packageName))
+                                    val sceneName = resources.getString(resources.getIdentifier("SceneName_$sceneTag", "string", it.packageName))
+
+                                    val asset = context.packageManager.getResourcesForApplication(it.packageName).assets.open("thumbs/${sceneTag}/Scene_${sceneTag}_1_1.png")
+
+                                    ElevatedCard(
+                                        elevation = CardDefaults.cardElevation(
+                                            defaultElevation = 2.dp
+                                        ),
+                                        modifier = Modifier
+                                            .padding(4.dp)
+                                            .fillMaxWidth()
+                                            .height(180.dp)
+                                            .width(200.dp)
+                                            .clickable(onClick = {
+                                                Settings.Global.putString(context.contentResolver, "SceneManager.CurPackage", it.packageName)
+                                                Settings.Global.putString(context.contentResolver, "SceneManager.CurrentScene", sceneTag)
+                                                Settings.Global.putString(context.contentResolver, "current_scene", "/assets/scene/$sceneTag/Scene_${sceneTag}_1_1.unity3d")
+                                                restartShell()
+                                            })
+                                    ) {
+
+                                        GlideImage(
+                                            model = BitmapFactory.decodeStream(asset),
+                                            contentDescription = null,
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .height(150.dp)
+                                                .width(200.dp),
+                                            contentScale = ContentScale.Crop
+                                        )
+
+                                        Row(
+                                            modifier = Modifier.padding(4.dp)
+                                        ) {
+                                            Text(
+                                                text = sceneName,
+                                                textAlign = TextAlign.Start,
+                                                maxLines = 1,
+                                                overflow = TextOverflow.Ellipsis
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
